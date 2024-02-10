0c521d71a9e377d5dfb53d069ab8196139dd42b0
Ronaldo Junior <ronaldomjunior05@gmail.com>
2024-02-10 15:21:44 -0300

    feat: add cors to routes

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 require("express-async-errors")
+const uploadConfig = require("./configs/upload")
+const { errorMiddleware, authMiddleware } = require("./middlewares")
+
 const express = require("express")
 const routes = require("./routes")
-const { errorMiddleware, authMiddleware } = require("./middlewares")
-const uploadConfig = require("./configs/upload")
+const cors = require("cors")
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER))
 app.use(authMiddleware)
 app.use(routes)
